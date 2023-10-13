e30e7d095bc7b49ee6665bcdb8c5b4516e14e26f
Bobby Cahill <bobby@bobbycahill.com>
2023-10-13 12:18:58 -0400

    Add production WSGI server support
    Also add contributing info to README
    
    Still need to add SSL support and support for
    
     port 80 without root

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,7 +5,7 @@ from snaarf_app.server import app, index
 
 
 def test_server_index_returns_rendered_template():
-    """ Just check for an expected piece of the result """
+    """Just check for an expected piece of the result"""
     with app.app_context(), app.test_request_context():
         indexOutput = index()
         assert "<title>SnaarfBot</title>" in indexOutput
--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,8 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from snaarf_app.server import app
 
 load_dotenv()
-
-app = Flask(__name__)
-
-
-@app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html", title="SnaarfBot")
-
 
 if __name__ == "__main__":
     app.run(host=os.getenv("PROD_HOST"), port=os.getenv("PROD_PORT"))
