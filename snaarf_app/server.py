c09456849f17221a29bc81956d4ac0463351f4c8
Bobby Cahill <bobby@bobbycahill.com>
2023-10-23 15:50:21 -0400

    Whoops

--- snaarf_app/server.py
+++ snaarf_app/server.py
@@ -13,5 +13,5 @@ def index():
     return render_template("index.html", title="SnaarfBot")
 
 
-if __name__ != "__main__":
+if __name__ == "__main__":
     app.run(host=os.getenv("DEV_HOST"), port=os.getenv("DEV_PORT"))
